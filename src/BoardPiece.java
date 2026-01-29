cfdfa9e31282162c3f4febdb1889f4b029f81e46
ItsGetWreckt <samanyu.rayala@gmail.com>
2026-01-28 17:50:59 -0800

    Changes

--- src/BoardPiece.java
+++ src/BoardPiece.java
@@ -1,23 +1,69 @@
 import java.util.ArrayList;
 
 public class BoardPiece {
-    public int xPos, yPos;
-    public final int SIZE;
-    public boolean isWhite;
-    public int x, y;
-    public boolean hasMoved;
+    private int xPos, yPos;
+    private boolean isWhite;
+    private int x, y;
+    private boolean hasMoved;
 
     public BoardPiece(int xPos, int yPos, boolean isWhite, int size) {
         this.xPos = xPos;
         this.yPos = yPos;
-        this.SIZE = size;
         x = xPos * size;
         y = yPos * size;
         this.isWhite = isWhite;
         hasMoved = false;
     }
 
-    public ArrayList<Vector2d> getLegalSquares(Game board) {
+    public ArrayList<Vector2d> getAttackSquares(Game board) {
         return new ArrayList<>();
+    }
+
+    public ArrayList<Vector2d> getLegalMoves(Game board) {
+        return new ArrayList<>();
+    }
+
+    public boolean isWhite() {
+        return isWhite;
+    }
+
+    public boolean hasMoved() {
+        return hasMoved;
+    }
+
+    public void setHasMoved(boolean hasMoved) {
+        this.hasMoved = hasMoved;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getXPos() {
+        return xPos;
+    }
+
+    public void setXPos(int xPos) {
+        this.xPos = xPos;
+    }
+
+    public int getYPos() {
+        return yPos;
+    }
+
+    public void setYPos(int yPos) {
+        this.yPos = yPos;
     }
 }
